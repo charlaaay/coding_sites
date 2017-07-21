c9cdf53cde90bd55e82acacb90b1446841480d11
Charlie Curtis <charlie-curtis@outlook.com>
2017-07-20 22:20:18 -0400

    do fizz buzz

---
+++ leetcode/412/Solution.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Solution {
+    public List<String> fizzBuzz(int n) {
+        
+        List<String> l = new ArrayList<String>();
+        
+        for (int i = 1; i<=n; i++) {
+            if (i % 15 == 0) {
+                l.add("FizzBuzz");
+            } else if (i % 5 == 0) {
+                l.add("Buzz");
+            } else if (i % 3 == 0) {
+                l.add("Fizz");
+            } else {
+                l.add(Integer.toString(i));
+            }
+        }
+        return l;
+    }
+}
