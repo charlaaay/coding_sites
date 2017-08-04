e43aae4b5e027ebb2f08f2f3bfd7a4b08f60503d
Charlie Curtis <charlie-curtis@outlook.com>
2017-08-04 00:46:07 -0400

    code up fun prob

---
+++ leetcode/561/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class Solution {
+    public int arrayPairSum(int[] nums) {
+        
+        Arrays.sort(nums);
+        int output = 0;
+        
+        for (int i = 0; i< nums.length; i = i+2) {
+           output += nums[i];
+        }
+        return output;
+        
+    }
+}
