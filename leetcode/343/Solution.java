5e5caad409cbedd27d4e9a7bc38541fa6b5462dc
Charlie Curtis <charlie-curtis@outlook.com>
2018-04-01 16:42:19 -0400

    Comment the code

--- leetcode/343/Solution.java
+++ leetcode/343/Solution.java
@@ -1,8 +1,4 @@
 /**
- * 
- */
-
-/**
  * @author charlie2ez
  *
  */
@@ -11,12 +7,14 @@ public class Solution {
 	public int integerBreak(int n) {
 		
 		if (n <= 3) {
+			//this is necessary because of the assumption made on line 18
 			return n-1;
 		}
 		
 		int[] maxProds = new int[n+1];
 		int num1, num2, num1Prod, num2Prod, currentValue;
 		for (int i = 0; i<= n; i++) {
+			//the max prod will atleast be the number. This holds true for n >= 4
 			maxProds[i] = i;
 			//System.out.println("Starting to compute n = " + i);
 			for (int j = 1; j<=i/2; j++) {
@@ -25,6 +23,9 @@ public class Solution {
 				//System.out.println("Starting to compute combo (" + num1 + "," + num2 + ")");
 				num1Prod = maxProds[num1];
 				num2Prod = maxProds[num2];
+				//It's not guaranteed that the product of the sum of a number will be greater
+				//than the number itself, so we must check both values. Specifically, n = {2,3}
+				//won't have a product greater than the number itself
 				currentValue = Math.max(num1 * num2, num1Prod * num2Prod);
 				maxProds[i] = Math.max(currentValue, maxProds[i]);
 			}
@@ -36,6 +37,6 @@ public class Solution {
 	public static void main(String[] args) {
 		
 		Solution sol = new Solution();
-		System.out.println(sol.integerBreak(2));
+		System.out.println(sol.integerBreak(30));
 	}
 }
