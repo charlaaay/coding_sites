95713eb0cb3caf28439a60d19c0f91bb15f21e05
Charlie Curtis <charlie-curtis@outlook.com>
2018-10-20 01:19:31 -0400

    stop copying array to fix TLE

--- leetcode/494/Solution.java
+++ leetcode/494/Solution.java
@@ -3,12 +3,12 @@ class Solution {
         if (nums.length == 0) {
             return 0;
         }
-        return doRecursion(nums, S, 0);
+        return doRecursion(nums, 0, S, 0);
     }
     
     //caller must handle empty arrays
-    private int doRecursion(int[] nums, int target, int current) {
-        if (nums.length == 0) {
+    private int doRecursion(int[] nums, int currentIndex, int target, int current) {
+        if (nums.length == currentIndex) {
             if (current == target) {
                 return 1;
             }
@@ -17,10 +17,10 @@ class Solution {
         int count = 0;
         //assume this value is negative, so subtract from current
         //slice off the first element and repeat
-        count += doRecursion(Arrays.copyOfRange(nums, 1, nums.length), target, current - nums[0]);
+        count += doRecursion(nums, currentIndex + 1, target, current - nums[currentIndex]);
         //assume this value is positve, so add to current
         //slice off the first element and repeat
-        count += doRecursion(Arrays.copyOfRange(nums, 1, nums.length), target, current + nums[0]);
+        count += doRecursion(nums, currentIndex + 1, target, current + nums[currentIndex]);
         return count;
     }
 }
