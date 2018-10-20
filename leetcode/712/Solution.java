090cbe7b02ff918998796d8899f934489e9f83ce
Charlie Curtis <charlie-curtis@outlook.com>
2018-10-20 01:11:34 -0400

    update problems

--- leetcode/712/Solution.java
+++ leetcode/712/Solution.java
@@ -8,11 +8,11 @@ class Solution {
         holder[holder.length -1][holder[0].length -1] = 0;
         for (int i = holder.length-1; i >= 0; i--) {
           for (int j = holder[i].length-1; j >= 0; j--) {
-            System.out.println("Starting with values (i,j) = " + i + ", " + j);
+            
             if ((i == holder.length - 1) && (j == holder[i].length -1)) {
               //this value has already been initialized
-              System.out.println("Min value for " + i + "," + j + " is " + holder[i][j]);
-              System.out.println("skipping base case");
+            
+            
               continue;  
             }
 
@@ -21,29 +21,21 @@ class Solution {
             //and bottom right neighbor. Pick the minimum after considerating delete/skip
          
             //let this represent your bottom neighbor. to get here, you would need to delete the character
-            //have to stop the index early because the limiting factor is s2.charAt(j+1)
-            //that is why we are doing length-1 and not just length
+            
             if (i+1 < holder.length) {
-              System.out.println("ENTERED CASE 1");
+              
               if (holder[i+1][j] + s2.charAt(i) < minValue) {
-                System.out.println("CASE 1");
+                
                 minValue = holder[i+1][j] + s2.charAt(i);
               }
             }
             //let this represent your right neighbor. to get here, you would need to delete a character
-            //have to stop the index early because the limiting factor is s1.charAt(j+1)
-            //that is why we are doing length-1 and not just length
             if (j+1 < holder[i].length) {
-              System.out.println("ENTERED CASE 2");
-              System.out.println("MinVal: " + minValue + " versus " + holder[i][j+1] + " and " + s1.charAt(j));
-              System.out.println("and i and j are " + i + " " + j);
               if(holder[i][j+1] + s1.charAt(j) < minValue) {
-                System.out.println("CASE 2");
                 minValue = holder[i][j+1] + s1.charAt(j);
               }
             }
             if ((j+1 < holder[i].length-1) && (i+1 < holder.length-1)) {
-              System.out.println("ENTERED CASE 3");
               int possibleMin = holder[i+1][j+1];
               char c = s2.charAt(i);
               char d  = s1.charAt(j);
@@ -52,20 +44,12 @@ class Solution {
                 possibleMin += c + d;
               }
               if (possibleMin < minValue) {
-                System.out.println("CASE 3");
                 minValue = possibleMin;
               }
             }
-            System.out.println("Min value for " + i + "," + j + " is " + minValue);
             holder[i][j] = minValue;
           }
         }
       return holder[0][0];
-    }
-
-    public static void main(String[] args) {
-
-      Solution sol = new Solution();
-      System.out.println(sol.minimumDeleteSum("food", "dog"));      
     }
 }
