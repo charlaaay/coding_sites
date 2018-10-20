d942979786e151a810fe94b70ff1828e7862c8ae
Charlie Curtis <charlie-curtis@outlook.com>
2018-10-20 01:42:27 -0400

    fix bug and submit my original solution. runtime reduced by 60%

--- leetcode/494/Solution.java
+++ leetcode/494/Solution.java
@@ -7,20 +7,25 @@ class Solution {
     }
     
     //caller must handle empty arrays
-    private int doRecursion(int[] nums, int currentIndex, int target, int current) {
-        if (nums.length == currentIndex) {
-            if (current == target) {
-                return 1;
-            }
-            return 0;
-        }
+    private int doRecursion(int[] nums, int startIndex, int target, int current) {
         int count = 0;
+        if (nums.length == startIndex+1) {
+            //if the value is 0, it will hit both of these base cases.
+            if (current - nums[startIndex] == target) {
+                count++;
+            }
+            if (current + nums[startIndex] == target) {
+                count++;
+            }
+            return count;
+        }
+        
         //assume this value is negative, so subtract from current
         //slice off the first element and repeat
-        count += doRecursion(nums, currentIndex + 1, target, current - nums[currentIndex]);
+        count += doRecursion(nums, startIndex+1, target, current - nums[startIndex]);
         //assume this value is positve, so add to current
         //slice off the first element and repeat
-        count += doRecursion(nums, currentIndex + 1, target, current + nums[currentIndex]);
+        count += doRecursion(nums, startIndex+1, target, current + nums[startIndex]);
         return count;
     }
 }
