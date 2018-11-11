118414f09c86b08e2c0cd864cf7d1dfa569eec12
Charlie Curtis <charlie-curtis@outlook.com>
2018-11-11 12:28:00 -0500

    441

---
+++ leetcode/441/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        int i = 1;
+        while (n > 0) {
+            n -= i;
+            i++;
+        }
+        
+        if (n == 0) {
+            return i-1;
+        }
+        return (i-2);
+    }
+}
